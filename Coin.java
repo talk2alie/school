be0ce4c772104f8730f68b98e00d491df2551560
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-10-27 16:59:47 -0400

    Add Coin enum

---
+++ Coin.java
@@ -0,0 +1,22 @@
+public enum Coin {
+    Penny("Copper", 1),
+    Dime("Silver", 10),
+    Nickel("Silver", 5),
+    Quarter("Silver", 25);
+
+    private final String color;
+    private final int value;
+
+    Coin(String color, int value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    double value() {
+        return value / 100.0;
+    }
+
+    String color() {
+        return color;
+    }
+}
--- WeekDayDriver.java
+++ WeekDayDriver.java
@@ -1,8 +1,8 @@
 public class WeekDayDriver {
     public static void main(String[] args) {
-        WeekDay day = WeekDay.SATURDAY;
-
-        boolean isWeekDay = day.isWeekDay();
-        System.out.print(isWeekDay);
+        Coin coin = Coin.Dime;
+        String color = coin.color();
+        double value = coin.value();
+        System.out.println(String.format("Color: %s, Value: %.2f", color, value));
     }
 }
