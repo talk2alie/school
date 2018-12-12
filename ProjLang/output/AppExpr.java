a5cc3afdbc5d4fcf5b352cac553f32a3b40e11e6
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-12-12 05:31:41 -0500

    Add bogus implementation of LetFunExpr

--- ProjLang/output/AppExpr.java
+++ ProjLang/output/AppExpr.java
@@ -1,14 +1,26 @@
 public class AppExpr extends Expr {
-    // public final String functionName;
-    // public final Expr paramExpr;
+    public final String paramName;
+    public final Expr paramExpr;
+    public final Expr functionExpr;
+
+    public AppExpr(String paramName, Expr paramExpr, Expr functionExpr) {
+        this.paramName = paramName;
+        this.paramExpr = paramExpr;
+        this.functionExpr = functionExpr;
+    }
 
     @Override
     public Value eval(Env e) throws EvalError {
-        return null;
+        Value paramValue = paramExpr.eval(e);
+        e = e.addBinding(paramName, paramValue);
+
+        return functionExpr.eval(e);
     }
 
     @Override
     public String toString() {
-        return null;
+        return paramName + " " +
+               paramExpr.toString() + " " +
+               functionExpr.toString;
     }
 }
